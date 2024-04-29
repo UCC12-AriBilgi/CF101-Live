f1387310ba470255bfebb3de5e09e76130141b09
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-29 10:52:04 +0300

    Some changes

---
+++ P01_CF/Data/StudentDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_CF.Entities;
+
+namespace P01_CF.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // tüm context ile ilgili olarak gerekiyorsa gerekli konfigüre işlemleri için yazılacak olan kodların bulunduğu bölüm
+
+            // Database Connection string konfigürasyonu gibi
+
+            optionsBuilder.UseSqlServer();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // burası modellerimiz yaratılırken eğer yapılacak işlemler varsa bu bölüme yazılır..
+        }
+
+        // Db tarafında yer alacak olan tablolarımızın model tanımlarını tanımlıyoruz...DbSet <--> table(db tarafında)
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+
+    }
+}
---
+++ P01_CF/Entities/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_CF.Entities
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        // Data Annotations
+
+        [Column(TypeName = "NVarchar(20)")]
+        public string SFName { get; set; }
+
+        [Column(TypeName = "NVarchar(30)")]
+        public string SLName { get; set; }
+
+        public DateTime SDoB {  get; set; }
+
+        
+
+
+
+
+    }
+}
