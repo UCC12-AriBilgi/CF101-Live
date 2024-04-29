6d3fd81ed2edc20dc2985d14172e65f7ce5c35ac
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-29 13:30:28 +0300

    Some changes

---
+++ P02_CF_wFakeData/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P02_CF_wFakeData.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
