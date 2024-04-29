6ad90640b78c54568654e4bff7ba6321d12fc58a
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-29 10:28:10 +0300

    Add project files.

---
+++ P01_CF/Entities/GenderENum.cs
@@ -0,0 +1,9 @@
+﻿namespace P01_CF.Entities
+{
+    public enum GenderENum
+    {
+        E,
+        K
+
+    }
+}
---
+++ P01_CF/Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace P01_CF.Entities
+{
+    public class Person
+    {
+        // Olmayan bir DB üzerinde yer alacak bir tableın(Person) görünümünü code tarafında gerçekleştiriyorum.
+
+        public int PersonId { get; set; }
+        public string PFName { get; set; }
+        public string PLName { get; set; }
+        public string PEMail {  get; set; }
+        public string PPhone { get; set; }
+        public string PAddress { get; set; }
+        public string PPostCode { get; set; }
+        public string PCity { get; set; }
+        public GenderENum PGender { get; set; }
+    }
+}
