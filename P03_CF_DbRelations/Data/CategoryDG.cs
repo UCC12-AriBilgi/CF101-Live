15d660278f303a66b72312da2df135f64a167497
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-30 14:57:57 +0300

    Some changes

---
+++ P03_CF_DbRelations/Data/CategoryDG.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Bogus;
+using Microsoft.EntityFrameworkCore;
+using P03_CF_DbRelations.Models;
+
+namespace P03_CF_DbRelations.Data
+{
+    public class CategoryDG
+    {
+        // Category Fake Data Generator
+
+        Faker<Category> fakerCategory; // Category iöçin FakeData
+
+        public DbSet<Category> Categories; // Categories sınıfından Categories isimli bir tabloyu ifade ediyor...
+
+        // constructor
+        public CategoryDG()
+        {
+            fakerCategory = new Faker<Category>();
+
+            fakerCategory.RuleFor(c => c.CategoryName, cd => cd.Commerce.Categories(1)[0]);
+        }
+
+        public void Generate(int count)
+        {
+            using var context=new // **** 
+        }
+
+
+
+
+    }
+}
