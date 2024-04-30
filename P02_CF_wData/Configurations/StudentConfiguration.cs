af7ef954ae01660b8a72bd62e3d5a859b041f3d1
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-30 11:50:02 +0300

    Some changes

---
+++ P02_CF_wData/Configurations/StudentConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P02_CF_wData.Entities;
+
+namespace P02_CF_wData.Configurations
+{
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.HasKey(x=> x.ID); // Bu alan benim key alanım olacak
+            builder.Property(x => x.ID).UseIdentityColumn(); // Bu alan otomatik olarak db tarafından artırılacak.
+            builder.Property(x=> x.Name).IsRequired(); // Bu alanım gerekli bir alandır.
+            builder.Property(x=> x.Name).HasMaxLength(30);// bu alanımın alacağı max değer
+            builder.Property(x => x.LastName).HasMaxLength(50);
+
+            builder.ToTable("Students"); // bu modelimin vt tarafında oluşturacağı tablo Students olsun.
+
+        }
+    }
+}
---
+++ P02_CF_wData/Configurations/TeacherConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P02_CF_wData.Entities;
+
+namespace P02_CF_wData.Configurations
+{
+    public class TeacherConfiguration : IEntityTypeConfiguration<Teacher>
+    {
+        public void Configure(EntityTypeBuilder<Teacher> builder)
+        {
+            builder.HasKey(x => x.ID);
+            builder.Property(x => x.ID).UseIdentityColumn();
+            builder.Property(x=> x.Name).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(30);
+            //builder.Property(x=> x.Name).IsRequired().HasMaxLength(30); 13-14 ün birleşik hali
+            builder.Property(x=> x.LastName).IsRequired();
+            builder.Property(x => x.LastName).HasMaxLength(50);
+
+            builder.ToTable("Teachers"); // db tarafındaki tablo
+        }
+    }
+}
